068f23a9a2af0702258e35038b90e6c5196798db
Dmitriy Garbuz <dmigar@gmail.com>
2018-11-11 21:02:37 +0300

    Express backend introduced

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const ValueTree = require("./ValueTree");
+
+function sendFileContent(response, filePath) {
+  fs.readFile(path.join(__dirname, filePath), (err, data) => {
+    if (err) throw err;
+    response.send(data.toString());
+  });
+}
+
+// CONFIGURE EXPRESS APP
+const app = express();
+const port = 3000;
+
+// Home page
+app.get("/", (req, res) => {
+  sendFileContent(res, "index.html");
+});
+
+app.get("/data.json", (req, res) => {
+  sendFileContent(res, "data.json");
+});
+
+// Value Tree script
+app.get("/value-tree.js", (req, res) => {
+  sendFileContent(res, "ValueTree.js");
+});
+
+app.listen(port);
